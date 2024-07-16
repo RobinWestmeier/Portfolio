9a858586e185f948173b0292db9e68ad559f7183
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-07-16 09:03:38 +0200

    added impressum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },     
-    { path: 'privacy', component: PrivacyComponent }
+    { path: 'privacy', component: PrivacyComponent },
+    { path: 'impressum', component: ImpressumComponent },
 ];
--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -17,7 +17,7 @@ export class PortfolioComponent {
       title: 'Join',
       technologies: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      liveTestUrl: '',
+      liveTestUrl: 'https://gruppe-786.developerakademie.net/components/login/login.html',
       gitHubUrl: 'https://github.com/RobinWestmeier'
     },
     {
@@ -25,7 +25,7 @@ export class PortfolioComponent {
       title: 'El Pollo Loco',
       technologies: 'JavaScript | HTML | CSS',
       description: 'A simple Jump-and-Run game based on an object-oriented approach.',
-      liveTestUrl: '',
+      liveTestUrl: 'https://robin-westmeier.developerakademie.net/el-pollo-loco/index.html',
       gitHubUrl: 'https://github.com/RobinWestmeier'
     }
   ];
