f8e6c4b41d4412f5b05d6bdaae4da3a857a39ccd
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-07-17 13:04:44 +0200

    New responsive changes for translated text

--- src/app/main-content/profile/profile.component.ts
+++ src/app/main-content/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
 import { SharedService } from '../../shared.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { HeaderComponent } from '../../header/header.component';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
@@ -10,13 +10,44 @@ import { TranslateModule } from '@ngx-translate/core';
   standalone: true,
   imports: [TranslateModule, CommonModule, HeaderComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-  constructor(private sharedService: SharedService) {}
+export class ProfileComponent implements OnInit {
+
+  currentLang: string = 'en';
+  constructor(
+    private sharedService: SharedService,
+    private translateService: TranslateService,
+    private cdRef: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const lang = localStorage.getItem('lang') || 'en';
+      this.currentLang = lang;
+      this.translateService.use(lang);
+
+      this.translateService.onLangChange.subscribe(() => {
+        this.currentLang = this.translateService.currentLang;
+        this.cdRef.detectChanges(); // Manuelles Triggern der Ansicht-Aktualisierung
+      });
+    }
+  }
+
+  changeLang(lang: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('lang', lang);
+      this.translateService.use(lang);
+    }
+  }
 
   scrollTo(targetId: string): void {
     this.sharedService.scrollTo(targetId);
+  }
+
+  isGER(): boolean {
+    return this.currentLang === 'de';
   }
 
   openGithub() {
@@ -24,9 +55,6 @@ export class ProfileComponent {
   }
 
   openLinkedIn() {
-    window.open(
-      'https://www.linkedin.com/',
-      '_blank'
-    );
+    window.open('https://www.linkedin.com/', '_blank');
   }
 }
