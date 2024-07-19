2cb02eb8ed06f651ab051153178f08eb1f8753ab
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-07-19 10:34:51 +0200

    small fixes

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SharedService } from '../shared.service';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-privacy',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent {
+  constructor(
+    private sharedService: SharedService,
+    private storageService: StorageService,
+  ) {}
 
+  @HostListener('click', ['$event'])
+  onClick(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (
+      target.tagName === 'A' &&
+      target.getAttribute('href')?.startsWith('#')
+    ) {
+      event.preventDefault();
+      const targetId = target.getAttribute('href')!.substring(1);
+      this.scrollTo(targetId);
+    }
+  }
+  
+  scrollTo(targetId: string): void {
+    this.sharedService.scrollTo(targetId);
+  }
 }
